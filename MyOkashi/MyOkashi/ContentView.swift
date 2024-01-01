ad4e28fab9b227e37436502a7d14cfdd2159cf03
hidepon503 <blue.passion625@gmail.com>
2024-01-01 19:07:29 +0900

    1st commit

---
+++ MyOkashi/MyOkashi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyOkashi
+//
+//  Created by 飯塚 秀幸 on 2024/01/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyOkashi/MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by 飯塚 秀幸 on 2024/01/01.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
